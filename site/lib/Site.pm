4f9dbb48e9100789f28ab89ad236a956f6e34de6
Blabos de Blebe <blabos@blabos.org>
2012-01-21 14:48:41 -0200

    Conversão da página Iniciantes. close #5

--- site/lib/Site.pm
+++ site/lib/Site.pm
@@ -15,7 +15,7 @@ sub startup {
     $self->plugin('PODRenderer');
 
     # Loading Modules
-    my @core_modules = qw{ Home };
+    my @core_modules = qw{ Home Iniciantes };
     Site::Loader::load( $self, @core_modules );
 }
 
